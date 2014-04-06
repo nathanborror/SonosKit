df043666d2ba84df5d3c6f37c90a3998c6eaa912
Nathan Borror <nathan@nathanborror.com>
2014-04-05 22:31:12 -0700

    Creating some test speakers if you're using the simulator on a network where there aren't any speakers available.

--- SonosKit/SonosDiscovery.m
+++ SonosKit/SonosDiscovery.m
@@ -27,8 +27,13 @@ typedef void (^kFindControllersBlock)(NSArray *ipAddresses);
       NSMutableArray *controllers = [[NSMutableArray alloc] init];
 
       if (ipAddresses.count == 0) {
+#if TARGET_IPHONE_SIMULATOR
+        completion([discover testControllers], nil);
+        return;
+#else
         completion(nil, nil);
         return;
+#endif
       }
 
       NSString *ipAddress = [ipAddresses objectAtIndex:0];
@@ -44,7 +49,6 @@ typedef void (^kFindControllersBlock)(NSArray *ipAddresses);
         NSArray *inputs = responseDict[@"ZPSupportInfo"][@"ZonePlayers"][@"ZonePlayer"];
 
         for (NSDictionary *input in inputs) {
-
           NSString *ipLocation = input[@"location"];
           NSRegularExpression *ipRegex = [NSRegularExpression regularExpressionWithPattern:@"\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}" options:0 error:nil];
           NSTextCheckingResult *ipRegexMatch = [ipRegex firstMatchInString:ipLocation options:0 range:NSMakeRange(0, ipLocation.length)];
@@ -53,14 +57,16 @@ typedef void (^kFindControllersBlock)(NSArray *ipAddresses);
 
           SonosController *controller = [[SonosController alloc] initWithIP:ip];
 
-          [controllers addObject:@{
-                                   @"ip": ip,
-                                   @"name": input[@"text"],
-                                   @"coordinator": [NSNumber numberWithBool:coordinator],
-                                   @"uuid": input[@"uuid"],
-                                   @"group": input[@"group"],
-                                   @"controller": controller
-                                   }];
+          if (![input[@"text"] isEqualToString:@"Sonos Bridge"]) {
+            [controllers addObject:@{
+                                     @"ip": controller.ip,
+                                     @"name": input[@"text"],
+                                     @"coordinator": [NSNumber numberWithBool:coordinator],
+                                     @"uuid": input[@"uuid"],
+                                     @"group": input[@"group"],
+                                     @"controller": controller
+                                     }];
+          }
         }
 
         completion(controllers, error);
@@ -104,6 +110,48 @@ typedef void (^kFindControllersBlock)(NSArray *ipAddresses);
   [_udpSocket close];
   _udpSocket = nil;
   _completionBlock(_ipAddresses);
+}
+
+- (NSArray *)testControllers
+{
+  NSMutableArray *controllers = [[NSMutableArray alloc] init];
+
+  [controllers addObject:@{
+                           @"ip": @"10.0.1.1",
+                           @"name": @"Living Room",
+                           @"coordinator": @1,
+                           @"uuid": @"RINCON_000E58D0540801400",
+                           @"group": @"RINCON_000E58D0540801400",
+                           @"controller": [[SonosController alloc] initWithIP:@"10.0.1.1"]
+                           }];
+
+  [controllers addObject:@{
+                           @"ip": @"10.0.1.2",
+                           @"name": @"Bedroom",
+                           @"coordinator": @1,
+                           @"uuid": @"RINCON_000E58898D4C01400",
+                           @"group": @"RINCON_000E58898D4C01400",
+                           @"controller": [[SonosController alloc] initWithIP:@"10.0.1.2"]
+                           }];
+
+  [controllers addObject:@{
+                           @"ip": @"10.0.1.3",
+                           @"name": @"Kitchen",
+                           @"coordinator": @0,
+                           @"uuid": @"RINCON_000E587BBA5201400",
+                           @"group": @"RINCON_000E58D0540801400",
+                           @"controller": [[SonosController alloc] initWithIP:@"10.0.1.3"]
+                           }];
+
+  [controllers addObject:@{
+                           @"ip": @"10.0.1.4",
+                           @"name": @"Bathroom",
+                           @"coordinator": @0,
+                           @"uuid": @"RINCON_000E587641F201400",
+                           @"group": @"RINCON_000E58D0540801400",
+                           @"controller": [[SonosController alloc] initWithIP:@"10.0.1.4"]
+                           }];
+  return [controllers copy];
 }
 
 #pragma mark - GCDAsyncUdpSocketDelegate
